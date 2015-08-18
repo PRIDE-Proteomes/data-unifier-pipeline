527ad1789a7222cef5da4c4070c9602fbf4102f4
noedelta <noedelta@gmail.com>
2015-08-18 18:24:55 +0100

    Updates the script to concatenate the isoforms with the canonical proteins (those were missing by mistake)

--- script/groups_generator.sh
+++ script/groups_generator.sh
@@ -4,13 +4,18 @@ echo -e "\nCleaning previously generated files\n"
 rm groups/*.txt
 rm proteins/*.fasta
 
-echo -e "\nDownloading uniprot current release proteomes\n"
+echo -e "\nDownloading uniprot current release proteomes proteins\n"
 
 # New ftp for proteomes in Uniprot
 curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000006548_3702_additional.fasta.gz"
 curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000005640_9606_additional.fasta.gz"
 curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000000589_10090_additional.fasta.gz"
 curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000002494_10116_additional.fasta.gz"
+
+curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000006548_3702.fasta.gz"
+curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000005640_9606.fasta.gz"
+curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000000589_10090.fasta.gz"
+curl -O "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000002494_10116.fasta.gz"
 
 echo -e "\nDownloading uniprot current release id mapping file\n"
 # New ftp for proteomes in Uniprot
@@ -26,10 +31,15 @@ gzip -d *.gz
 
 echo -e "\nRenaming fasta files to the convention pattern\n"
 
-mv UP000006548_3702_additional.fasta uniprot-taxonomy-3702-complete.fasta
-mv UP000005640_9606_additional.fasta uniprot-taxonomy-9606-complete.fasta
-mv UP000000589_10090_additional.fasta uniprot-taxonomy-10090-complete.fasta
-mv UP000002494_10116_additional.fasta uniprot-taxonomy-10116-complete.fasta
+mv UP000006548_3702.fasta uniprot-taxonomy-3702-complete.fasta
+mv UP000005640_9606.fasta uniprot-taxonomy-9606-complete.fasta
+mv UP000000589_10090.fasta uniprot-taxonomy-10090-complete.fasta
+mv UP000002494_10116.fasta uniprot-taxonomy-10116-complete.fasta
+
+cat UP000006548_3702_additional.fasta >> uniprot-taxonomy-3702-complete.fasta
+cat UP000005640_9606_additional.fasta >> uniprot-taxonomy-9606-complete.fasta
+cat UP000000589_10090_additional.fasta >> uniprot-taxonomy-10090-complete.fasta
+cat UP000002494_10116_additional.fasta >> uniprot-taxonomy-10116-complete.fasta
 
 echo -e  "\nGenerating Ensembl genes groups\n"
 
@@ -45,11 +55,18 @@ grep ">" uniprot-taxonomy-9606-complete.fasta | cut -d'|' -f 2,3 | cut -d' ' -f 
 grep ">" uniprot-taxonomy-10090-complete.fasta | cut -d'|' -f 2,3 | cut -d' ' -f 1 | awk -F'|' 'BEGIN{OFS="\t";}{t=$i;$i=$j;$j=t;aux=split(t,array,"-");$k=array[1]"\tENTRY";}1' i=1 j=3 k=1 | sort -k1 > entry-groups-10090-sorted.txt
 grep ">" uniprot-taxonomy-10116-complete.fasta | cut -d'|' -f 2,3 | cut -d' ' -f 1 | awk -F'|' 'BEGIN{OFS="\t";}{t=$i;$i=$j;$j=t;aux=split(t,array,"-");$k=array[1]"\tENTRY";}1' i=1 j=3 k=1 | sort -k1 > entry-groups-10116-sorted.txt
 
+echo -e "\nDeleting unused files\n"
+
+rm *.idmapping
+
+rm UP000006548_3702_additional.fasta
+rm UP000005640_9606_additional.fasta
+rm UP000000589_10090_additional.fasta
+rm UP000002494_10116_additional.fasta
+
+
 echo -e "\nMoving files to the right directory (relative routes)\n"
 
 mv *.txt groups
 mv *.fasta proteins
 
-echo -e "\nDeleting *.idmapping files\n"
-
-rm *.idmapping
