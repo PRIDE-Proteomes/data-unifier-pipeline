26552e075369c13af3e250c92d627bdcba1d964f
noedelta <noedelta@gmail.com>
2016-08-16 14:20:58 +0100

    Updates the group generator to load properly the isoforms and the canonical. This allow to store this information in the DB.

--- script/groups_generator.sh
+++ script/groups_generator.sh
@@ -61,25 +61,25 @@ gzip -d *.gz
 
 echo -e "\nRenaming fasta files to the convention pattern\n"
 
-mv UP000006548_3702.fasta uniprot-taxonomy-3702-complete.fasta
-mv UP000005640_9606.fasta uniprot-taxonomy-9606-complete.fasta
-mv UP000000589_10090.fasta uniprot-taxonomy-10090-complete.fasta
-mv UP000002494_10116.fasta uniprot-taxonomy-10116-complete.fasta
-mv UP000000437_7955.fasta uniprot-taxonomy-7955-complete.fasta
-mv UP000002311_559292.fasta uniprot-taxonomy-559292-complete.fasta
-mv UP000002485_284812.fasta uniprot-taxonomy-284812-complete.fasta
-mv UP000000803_7227.fasta uniprot-taxonomy-7227-complete.fasta
-mv UP000000625_83333.fasta uniprot-taxonomy-83333-complete.fasta
+mv UP000006548_3702.fasta uniprot-taxonomy-3702-proteins.fasta
+mv UP000005640_9606.fasta uniprot-taxonomy-9606-proteins.fasta
+mv UP000000589_10090.fasta uniprot-taxonomy-10090-proteins.fasta
+mv UP000002494_10116.fasta uniprot-taxonomy-10116-proteins.fasta
+mv UP000000437_7955.fasta uniprot-taxonomy-7955-proteins.fasta
+mv UP000002311_559292.fasta uniprot-taxonomy-559292-proteins.fasta
+mv UP000002485_284812.fasta uniprot-taxonomy-284812-proteins.fasta
+mv UP000000803_7227.fasta uniprot-taxonomy-7227-proteins.fasta
+mv UP000000625_83333.fasta uniprot-taxonomy-83333-proteins.fasta
 
-cat UP000006548_3702_additional.fasta >> uniprot-taxonomy-3702-complete.fasta
-cat UP000005640_9606_additional.fasta >> uniprot-taxonomy-9606-complete.fasta
-cat UP000000589_10090_additional.fasta >> uniprot-taxonomy-10090-complete.fasta
-cat UP000002494_10116_additional.fasta >> uniprot-taxonomy-10116-complete.fasta
-cat UP000000437_7955_additional.fasta >> uniprot-taxonomy-7955-complete.fasta
-cat UP000002311_559292_additional.fasta >> uniprot-taxonomy-559292-complete.fasta
-cat UP000002485_284812_additional.fasta >> uniprot-taxonomy-284812-complete.fasta
-cat UP000000803_7227_additional.fasta >> uniprot-taxonomy-7227-complete.fasta
-cat UP000000625_83333_additional.fasta >> uniprot-taxonomy-83333-complete.fasta
+mv UP000006548_3702_additional.fasta uniprot-taxonomy-3702-isoforms.fasta
+mv UP000005640_9606_additional.fasta uniprot-taxonomy-9606-isoforms.fasta
+mv UP000000589_10090_additional.fasta uniprot-taxonomy-10090-isoforms.fasta
+mv UP000002494_10116_additional.fasta uniprot-taxonomy-10116-isoforms.fasta
+mv UP000000437_7955_additional.fasta uniprot-taxonomy-7955-isoforms.fasta
+mv UP000002311_559292_additional.fasta uniprot-taxonomy-559292-isoforms.fasta
+mv UP000002485_284812_additional.fasta uniprot-taxonomy-284812-isoforms.fasta
+mv UP000000803_7227_additional.fasta uniprot-taxonomy-7227-isoforms.fasta
+mv UP000000625_83333_additional.fasta uniprot-taxonomy-83333-isoforms.fasta
 
 echo -e  "\nGenerating Ensembl genes mapping files\n"
 
@@ -87,7 +87,7 @@ grep "Ensembl" ARATH_3702_idmapping.dat > gene-map-3702.txt
 grep "Ensembl" HUMAN_9606_idmapping.dat > gene-map-9606.txt
 grep "Ensembl" MOUSE_10090_idmapping.dat > gene-map-10090.txt
 grep "Ensembl" RAT_10116_idmapping.dat > gene-map-10116.txt
-grep "Ensembl" DANRE_7955_idmapping.dat > gene-map-7995.txt
+grep "Ensembl" DANRE_7955_idmapping.dat > gene-map-7955.txt
 grep "Ensembl" YEAST_559292_idmapping.dat > gene-map-559292.txt
 grep "Ensembl" SCHPO_284812_idmapping.dat > gene-map-284812.txt
 grep "Ensembl" DROME_7227_idmapping.dat > gene-map-7227.txt
@@ -99,7 +99,7 @@ python utilities/mapper.py gene-map-3702.txt EnsemblGenome | sort -k1 > gene-gro
 python utilities/mapper.py gene-map-9606.txt Ensembl | sort -k1 > gene-groups-9606-sorted.txt
 python utilities/mapper.py gene-map-10090.txt Ensembl | sort -k1 > gene-groups-10090-sorted.txt
 python utilities/mapper.py gene-map-10116.txt Ensembl | sort -k1 > gene-groups-10116-sorted.txt
-python utilities/mapper.py gene-map-7995.txt Ensembl | sort -k1 > gene-groups-7995-sorted.txt
+python utilities/mapper.py gene-map-7955.txt Ensembl | sort -k1 > gene-groups-7955-sorted.txt
 python utilities/mapper.py gene-map-559292.txt EnsemblGenome | sort -k1 > gene-groups-559292-sorted.txt
 python utilities/mapper.py gene-map-284812.txt EnsemblGenome | sort -k1 > gene-groups-284812-sorted.txt
 python utilities/mapper.py gene-map-7227.txt EnsemblGenome | sort -k1 > gene-groups-7227-sorted.txt
@@ -110,16 +110,6 @@ echo -e "\nDeleting unused files\n"
 rm gene-map*
 rm *.dat
 #rm *.idmapping
-
-rm UP000006548_3702_additional.fasta
-rm UP000005640_9606_additional.fasta
-rm UP000000589_10090_additional.fasta
-rm UP000002494_10116_additional.fasta
-rm UP000000437_7955_additional.fasta
-rm UP000002311_559292_additional.fasta
-rm UP000002485_284812_additional.fasta
-rm UP000000803_7227_additional.fasta
-rm UP000000625_83333_additional.fasta
 
 echo -e "\nMoving files to the right directory (relative routes)\n"
 
